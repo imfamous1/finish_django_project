c66b6eab8ec08534d716fd411214d587114b6914
imfamous1 <107398098+imfamous1@users.noreply.github.com>
2025-03-26 21:26:05 +0300

    fix problem

--- mini_app/views.py
+++ mini_app/views.py
@@ -13,10 +13,10 @@ def save_user(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            user_id = data('user_id')
-            username = data('username', '')
-            first_name = data.get('first_name', '')
-            last_name = data.get('last_name', '')
+            user_id = data.get('user_id')
+            username = data.get('username', '')
+            first_name = data.get('first_name')
+            last_name = data.get('last_name')
 
             user, created = User.objects.update_or_create(
                 user_id=user_id,
@@ -24,16 +24,20 @@ def save_user(request):
                     'username': username,
                     'first_name': first_name,
                     'last_name': last_name
+                    # Другие поля можно оставить по умолчанию
                 }
             )
-
             return JsonResponse({
                 'status': 'success',
                 'user_id': user.user_id,
                 'created': created
             })
+
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
+
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
+
 
 
 def get_all_users(request):
